06af21cff9c4f914fd6be1f779b7d11c4b0d4c3d
Bhavesh Gupta <bgbhavesh@gmail.com>
2019-03-06 11:25:52 +0530

    test coverage
    100 percent

--- test/index.js
+++ test/index.js
@@ -1,11 +1,25 @@
 const test = require('tap').test;
-const {createSearchQuery} = require("../index");
+const { createSearchQuery, getAggregationArray } = require("../index");
 
 test('smoke test', function (t) {
-        var noSearch = createSearchQuery({});
-        console.log(noSearch)
-        t.is(noSearch,false);
-        noSearch = createSearchQuery({searchText=""});
-        console.log(noSearch)
+        var searchQuery = createSearchQuery({});
+        t.equal(searchQuery, false);
+        searchQuery = createSearchQuery({ searchText: "" });
+        t.equal(searchQuery, false);
+        searchQuery = createSearchQuery({ searchText: "test", searchFields: ['test'] });
+        t.equal(searchQuery["$or"].length, 1);
+        searchQuery = createSearchQuery({ searchText: "test", searchFields: ['test'], caseSensitive: true });
+        t.equal(searchQuery["$or"].length, 1);
+
+        var getAggregate = getAggregationArray();
+        t.equal(getAggregate.length, 4);
+        getAggregate = getAggregationArray({ project: { "test": 1 } });
+        t.equal(getAggregate.length, 5);
+        getAggregate = getAggregationArray({ skip: 0 }, { limit: 10 }, { project: { "test": 1 } });
+        t.equal(getAggregate.length, 4);
+        getAggregate = getAggregationArray({ searchText: "test", searchFields: ['test'], caseSensitive: true, skip: 0, limit: 10, project: { "test": 1 } });
+
+        t.equal(getAggregate.length, 5);
+        console.log(getAggregate);
         t.end();
 });
